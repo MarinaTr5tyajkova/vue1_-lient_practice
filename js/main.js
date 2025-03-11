437e139a92409acb421d3b76c88f1b3c17cf5743
MarinaTr5tyakova <tretyakowamarina26@gmail.com>
2025-03-11 12:56:03 +0700

    8.Calculated properties

--- js/main.js
+++ js/main.js
@@ -2,31 +2,49 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',
         altText: "A pair of socks",
-        inStock: false,
+        selectedVariant: 0,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
-        cart: 0
+        cart: 0,
+        onSale: true
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale() {
+            return this.onSale 
+                ? `${this.brand} ${this.product} are on sale!` 
+                : `${this.brand} ${this.product} are not on sale`;
+        }
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.cart += 1;
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
         }
     }
- })
- 
+});
